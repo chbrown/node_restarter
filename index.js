7aaa82a2994389ce8c72745729957747d93ac888
Christopher Brown <io@henrian.com>
2013-07-03 10:53:29 -0500

    De-nest, a little bit.

--- index.js
+++ index.js
@@ -17,20 +17,59 @@ function matchFiles(includes, excludes, callback) {
       callback(err1 || err2, _.uniq(includes_minus_excludes));
     });
   });
-}
-
-if (require.main === module) {
-  // don't use optimist because we can't tell optimist to keep
-  //  everything flat that's not a specified option.
-  var includes = ['**/*.js', '**/*.mu'];
-  // specified on the command line by a ! in front of them.
-  var excludes = ['node_modules/**/*.js', 'node_modules/**/*.mu'];
   // if (globpath[0] == '!') {
   //   excludes.push(minimatch.filter(globpath.slice(1)));
   // }
   // else {
   //   includes.push(globpath);
   // }
+}
+
+function run(command, args, filepaths) {
+  var child;
+
+  var change = function(filepath) {
+    child.kill();
+  };
+  var change_2000 = _.debounce(change, 2000, true);
+
+  var watchers = filepaths.map(function(filepath) {
+    return fs.watch(filepath, {persistent: false}, function(event) {
+      // event is either 'rename' or 'change'
+      console.error('[%d] file %sd: %s', Date.now(), event, filepath);
+      change_2000(filepath);
+    });
+  });
+  var unwatch = function() {
+    // unwatch all watched files
+    watchers.forEach(function(watcher) { watcher.close(); });
+  };
+
+  var start = function() {
+    console.error('[%d] child starting. %s %s', Date.now(), command, args.join(' '));
+    child = child_process.spawn(command, args, {stdio: 'inherit'});
+    child.on('exit', function(code, signal) {
+      console.error('[%d] child exited. code: %d, signal: %s', Date.now(), code, signal);
+      start_1000();
+    });
+  };
+  var start_1000 = _.debounce(start, 1000, false);
+
+  process.on('exit', function (code, signal) {
+    console.error('[%d] node_restarter exiting. code: %d, signal: %s', Date.now(), code, signal);
+    child.kill();
+    unwatch();
+  });
+
+  start();
+}
+
+function main() {
+  // don't use optimist because we can't tell optimist to keep
+  //  everything flat that's not a specified option.
+  var includes = ['**/*.js', '**/*.mu'];
+  // specified on the command line by a ! in front of them.
+  var excludes = ['node_modules/**/*.js', 'node_modules/**/*.mu'];
 
   // argv[0] = 'node', argv[1] = 'index.js', ...
   var command = process.argv[2];
@@ -39,34 +78,12 @@ if (require.main === module) {
   }
   var args = process.argv.slice(3);
 
-  console.log('- node_restarter: ' + command + ' ' + args.join(' '));
 
   matchFiles(includes, excludes, function(err, filepaths) {
-    var watchers = [];
-    var restart = function() {
-      watchers.forEach(function(watcher) { watcher.close(); });
-      var child = child_process.spawn(command, args, {stdio: 'inherit'});
-
-      var change_debounced = _.debounce(function(filepath) {
-        console.error(filepath + ' changed');
-        child.kill();
-      }, 250, true);
-
-      watchers = filepaths.map(function(filepath) {
-        return fs.watch(filepath, {persistent: false}, function(event) {
-          change_debounced(filepath);
-        });
-      });
-
-      child.on('exit', restart_500);
-
-      process.on('exit', function (code) {
-        child.kill();
-        watchers.forEach(function(watcher) { watcher.close(); });
-      });
-
-    };
-    var restart_500 = _.debounce(restart, 500, true);
-    restart();
+    console.log('node_restarter watching %d files', filepaths.length);
+    if (err) throw err;
+    run(command, args, filepaths);
   });
 }
+
+if (require.main === module) main(); // closures are nice
